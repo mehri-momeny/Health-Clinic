9b2eed9cf0b859d32e94318a21216b6fc1b15824
mehrnaz m <mehrnaz.m69@hotmail.com>
2021-08-31 10:51:42 +0430

    Report form done

--- Patient clinic/New staff/frmReportNSTests.Designer.cs
+++ Patient clinic/New staff/frmReportNSTests.Designer.cs
@@ -28,9 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition2 = new Telerik.WinControls.UI.TableViewDefinition();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReportNSTests));
             this.groupPanel1 = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.cmbTest = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.txtNational_Code = new DevComponents.DotNetBar.Controls.TextBoxX();
             this.radGvReport = new Telerik.WinControls.UI.RadGridView();
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -39,11 +41,9 @@
             this.groupPanel2 = new DevComponents.DotNetBar.Controls.GroupPanel();
             this.btnexit = new DevComponents.DotNetBar.ButtonX();
             this.btnShow_Data = new DevComponents.DotNetBar.ButtonX();
-            this.btnExcel = new DevComponents.DotNetBar.ButtonX();
-            this.txtNational_Code = new DevComponents.DotNetBar.Controls.TextBoxX();
-            this.cmbTest = new DevComponents.DotNetBar.Controls.ComboBoxEx();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txttotalPoint = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbltotalPoint = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.groupPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.radGvReport)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radGvReport.MasterTemplate)).BeginInit();
@@ -58,17 +58,18 @@
             this.groupPanel1.Controls.Add(this.txtNational_Code);
             this.groupPanel1.Controls.Add(this.radGvReport);
             this.groupPanel1.Controls.Add(this.label3);
-            this.groupPanel1.Controls.Add(this.label6);
+            this.groupPanel1.Controls.Add(this.lbltotalPoint);
+            this.groupPanel1.Controls.Add(this.label2);
+            this.groupPanel1.Controls.Add(this.label4);
             this.groupPanel1.Controls.Add(this.label5);
             this.groupPanel1.Controls.Add(this.label1);
-            this.groupPanel1.Controls.Add(this.txttotalPoint);
             this.groupPanel1.Controls.Add(this.txtPoint);
             this.groupPanel1.Controls.Add(this.groupPanel2);
             this.groupPanel1.DisabledBackColor = System.Drawing.Color.Empty;
             this.groupPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupPanel1.Location = new System.Drawing.Point(0, 0);
             this.groupPanel1.Name = "groupPanel1";
-            this.groupPanel1.Size = new System.Drawing.Size(662, 398);
+            this.groupPanel1.Size = new System.Drawing.Size(931, 494);
             // 
             // 
             // 
@@ -99,9 +100,37 @@
             this.groupPanel1.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
             this.groupPanel1.TabIndex = 40;
             // 
+            // cmbTest
+            // 
+            this.cmbTest.DisplayMember = "Text";
+            this.cmbTest.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbTest.FormattingEnabled = true;
+            this.cmbTest.ItemHeight = 17;
+            this.cmbTest.Location = new System.Drawing.Point(107, 24);
+            this.cmbTest.Name = "cmbTest";
+            this.cmbTest.Size = new System.Drawing.Size(399, 23);
+            this.cmbTest.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cmbTest.TabIndex = 46;
+            this.cmbTest.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CmbTest_KeyDown);
+            // 
+            // txtNational_Code
+            // 
+            // 
+            // 
+            // 
+            this.txtNational_Code.Border.Class = "TextBoxBorder";
+            this.txtNational_Code.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtNational_Code.Location = new System.Drawing.Point(671, 24);
+            this.txtNational_Code.Name = "txtNational_Code";
+            this.txtNational_Code.PreventEnterBeep = true;
+            this.txtNational_Code.Size = new System.Drawing.Size(147, 22);
+            this.txtNational_Code.TabIndex = 45;
+            this.txtNational_Code.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtNational_Code_KeyDown);
+            this.txtNational_Code.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNational_Code_KeyPress);
+            // 
             // radGvReport
             // 
-            this.radGvReport.Location = new System.Drawing.Point(10, 109);
+            this.radGvReport.Location = new System.Drawing.Point(10, 127);
             // 
             // 
             // 
@@ -114,10 +143,10 @@
             this.radGvReport.MasterTemplate.AllowEditRow = false;
             this.radGvReport.MasterTemplate.AllowRowHeaderContextMenu = false;
             this.radGvReport.MasterTemplate.AllowRowReorder = true;
-            this.radGvReport.MasterTemplate.ViewDefinition = tableViewDefinition1;
+            this.radGvReport.MasterTemplate.ViewDefinition = tableViewDefinition2;
             this.radGvReport.Name = "radGvReport";
             this.radGvReport.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.radGvReport.Size = new System.Drawing.Size(642, 228);
+            this.radGvReport.Size = new System.Drawing.Size(906, 305);
             this.radGvReport.TabIndex = 40;
             this.radGvReport.ThemeName = "ControlDefault";
             // 
@@ -126,7 +155,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label3.Location = new System.Drawing.Point(343, 25);
+            this.label3.Location = new System.Drawing.Point(511, 27);
             this.label3.Name = "label3";
             this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.label3.Size = new System.Drawing.Size(47, 16);
@@ -139,7 +168,7 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label5.Location = new System.Drawing.Point(598, 67);
+            this.label5.Location = new System.Drawing.Point(841, 66);
             this.label5.Name = "label5";
             this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.label5.Size = new System.Drawing.Size(44, 16);
@@ -152,7 +181,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label1.Location = new System.Drawing.Point(581, 25);
+            this.label1.Location = new System.Drawing.Point(826, 27);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.label1.Size = new System.Drawing.Size(59, 16);
@@ -164,9 +193,9 @@
             // 
             this.txtPoint.Enabled = false;
             this.txtPoint.Font = new System.Drawing.Font("Tahoma", 9.75F);
-            this.txtPoint.Location = new System.Drawing.Point(482, 64);
+            this.txtPoint.Location = new System.Drawing.Point(773, 63);
             this.txtPoint.Name = "txtPoint";
-            this.txtPoint.Size = new System.Drawing.Size(34, 23);
+            this.txtPoint.Size = new System.Drawing.Size(45, 23);
             this.txtPoint.TabIndex = 42;
             // 
             // groupPanel2
@@ -175,11 +204,10 @@
             this.groupPanel2.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
             this.groupPanel2.Controls.Add(this.btnexit);
             this.groupPanel2.Controls.Add(this.btnShow_Data);
-            this.groupPanel2.Controls.Add(this.btnExcel);
             this.groupPanel2.DisabledBackColor = System.Drawing.Color.Empty;
-            this.groupPanel2.Location = new System.Drawing.Point(10, 344);
+            this.groupPanel2.Location = new System.Drawing.Point(10, 438);
             this.groupPanel2.Name = "groupPanel2";
-            this.groupPanel2.Size = new System.Drawing.Size(647, 45);
+            this.groupPanel2.Size = new System.Drawing.Size(912, 45);
             // 
             // 
             // 
@@ -223,6 +251,7 @@
             this.btnexit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btnexit.TabIndex = 37;
             this.btnexit.Text = "بازگشت";
+            this.btnexit.Click += new System.EventHandler(this.Btnexit_Click);
             // 
             // btnShow_Data
             // 
@@ -231,85 +260,67 @@
             this.btnShow_Data.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
             this.btnShow_Data.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
             this.btnShow_Data.Image = global::Patient_clinic.Properties.Resources.thin_0244_text_bullets_list;
-            this.btnShow_Data.Location = new System.Drawing.Point(535, 2);
+            this.btnShow_Data.Location = new System.Drawing.Point(796, 2);
             this.btnShow_Data.Name = "btnShow_Data";
             this.btnShow_Data.Size = new System.Drawing.Size(103, 34);
             this.btnShow_Data.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btnShow_Data.TabIndex = 35;
-            this.btnShow_Data.Text = "نمایش";
+            this.btnShow_Data.Text = "نمایش ";
+            this.btnShow_Data.Click += new System.EventHandler(this.BtnShow_Data_Click);
             // 
-            // btnExcel
+            // label2
             // 
-            this.btnExcel.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnExcel.BackColor = System.Drawing.Color.Transparent;
-            this.btnExcel.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnExcel.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnExcel.Image = ((System.Drawing.Image)(resources.GetObject("btnExcel.Image")));
-            this.btnExcel.Location = new System.Drawing.Point(269, 2);
-            this.btnExcel.Name = "btnExcel";
-            this.btnExcel.Size = new System.Drawing.Size(259, 34);
-            this.btnExcel.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.btnExcel.TabIndex = 36;
-            this.btnExcel.Text = "خروجی اکسل تمام بخش ها و فرم ها";
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(751, 66);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "از";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // txtNational_Code
+            // lbltotalPoint
             // 
+            this.lbltotalPoint.AutoSize = true;
+            this.lbltotalPoint.BackColor = System.Drawing.Color.Transparent;
+            this.lbltotalPoint.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lbltotalPoint.Location = new System.Drawing.Point(719, 66);
+            this.lbltotalPoint.Name = "lbltotalPoint";
+            this.lbltotalPoint.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbltotalPoint.Size = new System.Drawing.Size(20, 16);
+            this.lbltotalPoint.TabIndex = 38;
+            this.lbltotalPoint.Text = "...";
+            this.lbltotalPoint.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // label4
             // 
-            // 
-            this.txtNational_Code.Border.Class = "TextBoxBorder";
-            this.txtNational_Code.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.txtNational_Code.Location = new System.Drawing.Point(426, 23);
-            this.txtNational_Code.Name = "txtNational_Code";
-            this.txtNational_Code.PreventEnterBeep = true;
-            this.txtNational_Code.Size = new System.Drawing.Size(147, 22);
-            this.txtNational_Code.TabIndex = 45;
-            this.txtNational_Code.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNational_Code_KeyPress);
-            // 
-            // cmbTest
-            // 
-            this.cmbTest.DisplayMember = "Text";
-            this.cmbTest.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.cmbTest.FormattingEnabled = true;
-            this.cmbTest.ItemHeight = 17;
-            this.cmbTest.Location = new System.Drawing.Point(115, 19);
-            this.cmbTest.Name = "cmbTest";
-            this.cmbTest.Size = new System.Drawing.Size(223, 23);
-            this.cmbTest.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
-            this.cmbTest.TabIndex = 46;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label6.Location = new System.Drawing.Point(524, 67);
-            this.label6.Name = "label6";
-            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label6.Size = new System.Drawing.Size(13, 16);
-            this.label6.TabIndex = 38;
-            this.label6.Text = "/";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // txttotalPoint
-            // 
-            this.txttotalPoint.Enabled = false;
-            this.txttotalPoint.Font = new System.Drawing.Font("Tahoma", 9.75F);
-            this.txttotalPoint.Location = new System.Drawing.Point(539, 64);
-            this.txttotalPoint.Name = "txttotalPoint";
-            this.txttotalPoint.Size = new System.Drawing.Size(34, 23);
-            this.txttotalPoint.TabIndex = 42;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(774, 105);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(120, 16);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "پاسخ‌های اشتباه :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmReportNSTests
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 398);
+            this.ClientSize = new System.Drawing.Size(931, 494);
             this.Controls.Add(this.groupPanel1);
             this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmReportNSTests";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "گزارش آزمون";
             this.Load += new System.EventHandler(this.FrmReportNSTests_Load);
             this.groupPanel1.ResumeLayout(false);
@@ -332,10 +343,10 @@
         private DevComponents.DotNetBar.Controls.GroupPanel groupPanel2;
         private DevComponents.DotNetBar.ButtonX btnexit;
         private DevComponents.DotNetBar.ButtonX btnShow_Data;
-        private DevComponents.DotNetBar.ButtonX btnExcel;
         private DevComponents.DotNetBar.Controls.TextBoxX txtNational_Code;
         private DevComponents.DotNetBar.Controls.ComboBoxEx cmbTest;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txttotalPoint;
+        private System.Windows.Forms.Label lbltotalPoint;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Patient clinic/New staff/frmReportNSTests.cs
+++ Patient clinic/New staff/frmReportNSTests.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Telerik.WinControls.UI;
+//using Telerik.WinControls.UI.Export;
 
 namespace Patient_clinic.New_staff
 {
@@ -15,14 +17,10 @@ namespace Patient_clinic.New_staff
     {
         SqlConnection con = Program.CreateConnection();  //new SqlConnection("Data Source =(local) ; initial catalog = Health_clinic ; integrated security=true"); //Program.CreateConnection();
         SqlCommand cmd = new SqlCommand();
+        int S_ID = 0;
         public frmReportNSTests()
         {
             InitializeComponent();
-        }
-
-        private void TxtYear_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void TxtNational_Code_KeyPress(object sender, KeyPressEventArgs e)
@@ -53,6 +51,157 @@ namespace Patient_clinic.New_staff
             //cmbSurgeryType.SelectedIndex = 0;
 
             con.Close();
+        }
+
+        private void CmbTest_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Load_Data();
+                dgv_load();
+            }
+        }
+        void Load_Data()
+        {
+            S_ID = Get_S_ID(txtNational_Code.Text);
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.Parameters.Clear();
+                cmd.CommandText = "SELECT Point FROM NS_Test_Result WHERE S_ID = @S_ID AND T_ID = @T_ID";
+                cmd.Parameters.AddWithValue("S_ID", S_ID);
+                cmd.Parameters.AddWithValue("T_ID", cmbTest.SelectedValue);
+
+                txtPoint.Text = cmd.ExecuteScalar().ToString();
+
+
+                //total point of test 
+                cmd.Parameters.Clear();
+                cmd.CommandText = "SELECT [Total_Point] FROM [NS_Tests] WHERE ID = @T_ID";
+                cmd.Parameters.AddWithValue("T_ID", cmbTest.SelectedValue);
+
+                lbltotalPoint.Text = cmd.ExecuteScalar().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("مشکلی  پیش آمده است٬مجددا امتحان کنید\n" + ex.Message);
+            }
+            
+
+            con.Close();
+        }
+        int Get_S_ID(string National_Code)
+        {
+            int S_ID;
+            con.Open();
+            cmd.Connection = con;
+            try
+            {
+                cmd.CommandText = "select ID from [NS_Staffs] where National_code =\'" + txtNational_Code.Text + "\'";
+                S_ID = Convert.ToInt32(cmd.ExecuteScalar());
+               
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("مشکلی  پیش آمده است٬مجددا کد ملی را وارد نمایید\n" + e.Message);
+                txtNational_Code.Clear();
+                S_ID = 0;
+            }
+            con.Close();
+            return S_ID;
+        }
+
+        private void BtnShow_Data_Click(object sender, EventArgs e)
+        {
+            Load_Data();
+            dgv_load();
+        }
+
+        void dgv_load()
+        {
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+
+                SqlDataAdapter adp = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                adp.SelectCommand = new SqlCommand();
+                adp.SelectCommand.Connection = con;
+
+                adp.SelectCommand.CommandText = "SELECT  ROW_NUMBER() OVER(ORDER BY Q.ID ASC) Row_num,Q.Text, " +
+                                                       " CASE "+
+                                                        "    WHEN Answer = 1 THEN opt1 "+
+                                                        "    WHEN Answer = 2 THEN opt2 "+
+                                                        "   WHEN Answer = 3 THEN opt3 "+
+                                                        "    WHEN Answer = 4 THEN opt4 "+
+                                                        "    ELSE '' "+
+                                                        "END AS S_answer, "+
+                                                        "CASE "+
+                                                        "   WHEN Correct_Answer = 1 THEN opt1 "+
+                                                        "    WHEN Correct_Answer = 2 THEN opt2 "+
+                                                        "    WHEN Correct_Answer = 3 THEN opt3 "+
+                                                        "    WHEN Correct_Answer = 4 THEN opt4 "+
+                                                        "    ELSE '' "+
+                                                        "END AS C_answer "+
+                                                    "FROM NS_Question_Answers NSQA, MI_Q_Answer_Options MIQO,MI_Questions Q "+
+                                                    "WHERE NSQA.Q_ID = MIQO.Q_ID AND S_ID = @S_ID AND T_ID = @T_ID AND Answer != Correct_Answer AND NSQA.Q_ID = Q.ID";
+                adp.SelectCommand.Parameters.AddWithValue("S_ID", S_ID);
+                adp.SelectCommand.Parameters.AddWithValue("T_ID", cmbTest.SelectedValue);
+
+                adp.Fill(ds, "Report");
+
+                radGvReport.DataSource = ds;
+                radGvReport.DataMember = "Report";
+                Convert_column_name(radGvReport);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("مشکلی  پیش آمده است٬مجددا امتحان کنید\n" + ex.Message);
+            }
+           
+            con.Close();
+        }
+        private void Convert_column_name(RadGridView radGvReport)
+        {
+            radGvReport.TableElement.RowHeight = 30;
+            radGvReport.Columns["Row_num"].HeaderText = "ردیف ";
+            radGvReport.Columns["Row_num"].Width = 35;
+            radGvReport.Columns["Row_num"].TextAlignment = ContentAlignment.MiddleLeft;
+
+            radGvReport.Columns["Text"].HeaderText = "عنوان سوال ";
+            radGvReport.Columns["Text"].Width = 493;
+            radGvReport.Columns["Text"].WrapText = true;
+            radGvReport.Columns["Text"].TextAlignment = ContentAlignment.MiddleLeft;
+
+            radGvReport.Columns["S_answer"].HeaderText = "پاسخ انتخاب شده ";
+            radGvReport.Columns["S_answer"].Width = 170;
+            radGvReport.Columns["S_answer"].WrapText = true;
+            radGvReport.Columns["S_answer"].TextAlignment = ContentAlignment.MiddleCenter;
+
+            radGvReport.Columns["C_answer"].HeaderText = "پاسخ صحیح ";
+            radGvReport.Columns["C_answer"].Width = 170;
+            radGvReport.Columns["C_answer"].WrapText = true;
+            radGvReport.Columns["C_answer"].TextAlignment = ContentAlignment.MiddleCenter;
+
+        }
+
+        private void TxtNational_Code_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Load_Data();
+                dgv_load();
+            }
+        }
+
+
+        private void Btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
